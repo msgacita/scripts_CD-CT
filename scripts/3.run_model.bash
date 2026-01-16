1aabbcd14e67d45cafe442de7b43a200d0fc4be5
msgacita <madeleine.gacita@inpe.br>
2026-01-16 13:29:35 +0000

    Revert "Small changes for CD-CT in jaci" These changes were saved on branch jaci_dev_CD-CT This reverts commit 224a21e72c286659d5b8f35a266ca4c2330ad26d.

--- scripts/3.run_model.bash
+++ scripts/3.run_model.bash
@@ -73,8 +73,7 @@ cores=${MODEL_ncores}
 hhi=${YYYYMMDDHHi:8:2}
 NLEV=55
 CONFIG_CONV_INTERVAL="00:30:00"
-#VARTABLE=".OPER"
-VARTABLE=".TEST"
+VARTABLE=".OPER"
 export DIRRUN=${DIRHOMED}/run.${YYYYMMDDHHi}; rm -fr ${DIRRUN}; mkdir -p ${DIRRUN}
 #------------------------------------------------------------------------------------
 
